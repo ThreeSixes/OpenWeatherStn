da1595415db99379e9fc7e52cb42e669f5000309
ThreeSixes <actual@joshlucy.com>
2015-03-10 13:47:55 -0400

    Updated owsScanner to check the compound sensor for wind, rain, and light data.

--- rpi/scanner.py
+++ rpi/scanner.py
@@ -25,45 +25,105 @@ class owsScanner:
         # Anemometer ADC offset value
         self.windOffset = windOffset
         
-        # Initialize our sensor classes.
-        self.magSens = hmc5883l()
-        self.cmpdSens = compoundSensor(windOffset, 0x64)
+        # Set up our sensor objects
+        self.windDirSens = hmc5883l()
+        self.cmpdSens = compoundSensor(windOffset)
     
     def getWindDir(self, magOffset):
         """
         getWindDir(magOffset)
         
-        Get the heading of the wind in degrees. The magOffset should be the direction the sensor is facing. This offset will change the offset in the wind vein.
+        Get the heading of the wind in degrees. The magOffset should be the direction (heading) the sensor is facing. This offset will modify the wind vein's return data to reflect a correct wind heading. Returns an integer rounded to one decimal place.
         """
         
-        # Magnetic sensor data
+        # Magnetic sensor data (X, Z, Y)
         magData = [0, 0, 0]
         
         # Configure magnetometer
-        self.magSens.setReg(self.magSens.regCfgA, (self.magSens.avg1 | self.magSens.freq15 | self.magSens.biasNone))
-        self.magSens.setReg(self.magSens.regCfgB, self.magSens.gain230)
-        self.magSens.setReg(self.magSens.regMode, self.magSens.modeCont)
+        self.windDirSens.setReg(self.windDirSens.regCfgA, (self.windDirSens.avg1 | self.windDirSens.freq15 | self.windDirSens.biasNone))
+        self.windDirSens.setReg(self.windDirSens.regCfgB, self.windDirSens.gain230)
+        self.windDirSens.setReg(self.windDirSens.regMode, self.windDirSens.modeCont)
         
         # Get data from the magentometer.
-        magData = self.magSens.getXZY()
+        magData = self.windDirSens.getXZY()
         
-        # Compute heading as a cartesian value given data on the x, Y planes.
+        # Compute heading as a cartesian value given data on the X, Y planes. Heading is relative to the sensor, not north.
         heading = math.atan2(magData[0], magData[2]) * 180.0 / math.pi
         
         # Since the heading is reported from -180 to +180 make sure we compensate for that fact
-        # to return a normal heading from 0-359 degrees.
+        # to return a normal heading from 0 - 359 degrees.
         if heading < 0:
             heading = 360 - abs(heading)
         
-        # Add offset to heading
+        # Add offset to heading, to adjust for the weather station's orientation relative to north.
         offsetHeading = heading + offset
         
-        # If our offset heading wraps around the circle compensate for that.
+        # If our offset heading wraps around the heading "circle" compensate for it.
         if offsetHeading > 360:
             offsetHeading = offsetHeading - 360
         
         # Round to one decimal place, and return.
         return round(offsetHeading, 1)
+    
+    def getWindAvgSpeed(self):
+        """
+        getWindAvgSpeed()
+        
+        Gets the average wind speed from the compound sensor. Returns an integer expressing wind speed in kph, rounded to two digits or "None" if the sensor is not installed.
+        """
+        
+        retVal = None
+        
+        # If our sensor reports having an anemometer installed get the value.
+        if self.cmpdSens.checkStatusReg(self.cmpdSens.i2cStatus_wind):
+            retVal = self.cmpdSens.getWindAvg()
+        
+        return retVal
+    
+    def getWindMaxSpeed(self):
+        """
+        getWindMaxSpeed()
+        
+        Gets the max wind speed from the compound sensor. Returns an integer expressing wind speed in kph, rounded to two digits or "None" if the sensor is not installed.
+        """
+        
+        retVal = None
+        
+        # If our sensor reports having an anemometer installed get the value.
+        if self.cmpdSens.checkStatusReg(self.cmpdSens.i2cStatus_wind):
+            retVal = self.cmpdSens.getWindMax()
+        
+        return retVal
+    
+    def getRainCount(self):
+        """
+        getRainCount()
+        
+        Gets the rain counter value from the compound sensor. Returns a 32 bit integer or "None" if the sensor is not installed.
+        """
+        
+        retVal = None
+        
+        # If our sensor reports having the rain sensor installed get the value.
+        if self.cmpdSens.checkStatusReg(self.cmpdSens.i2cStatus_rain):
+            retVal = self.cmpdSens.getRainCount()
+        
+        return retVal
+    
+    def getAmbientLight(self):
+        """
+        getAmbientLight()
+        
+        Gets the ambient light average from the compound sensor module. Returns an 8-bit unsigned number or "None" if the light sensor is not installed.
+        """
+        retVal = None
+        
+        # If our sensor reports having the light sensor installed get the value.
+        if self.cmpdSens.checkStatusReg(self.cmpdSens.i2cStatus_light):
+            retVal = self.cmpdSens.getLightAvg()
+        
+        return retVal
+    
 
 #######################
 # Main execution body #
