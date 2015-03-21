9ec08c414cb03cca0270376872bb14a85abe9827
Josh Lucy <actual@joshlucy.com>
2015-03-21 15:01:47 -0400

    Updated weatherService.py to be run in a WSGI wrapper.

--- rpi/weatherService.py
+++ rpi/weatherService.py
@@ -8,47 +8,63 @@ import json
 import datetime
 from owsData import owsData
 from pprint import pprint
+from wsgiref.simple_server import make_server
+from wsgiref.validate import validator
 
 ########################
-# Main execution body #
+# weatherService class #
 ########################
 
-# Load data layer.
-dl = owsData()
-
-# Figoure out our mode.
-modeJson = True
-
-# Pull the last record as a tuple
-lastRecord = dl.getLastRecord()
-
-# Grab the date time stamp for processing as a string.
-dts = str(lastRecord[0])
-
-# Zero-fill the string in the event we don't have ms specified.
-# For consistency.
-if len(dts) == 19:
-    dts = dts + ".000000"
-
-def htmlify(weatherDict):
+class weatherService:
     """
-    htmlify(weatherDict)
-    
-    Convert data to an HTML page.
+    Simple HTTP service for handling weather data requests.
     """
-    # HTML header
-    body = "<HTML>\n<HEAD>\n<TITLE>OpenWeatherStn live data</TITLE>\n</HEAD>\n<BODY>\n"
-    
-    # Build page with data.
-    
-    # HTML footer
-    body = body + "</BODY>\n</HTML>"
-    
-    # Dump page.
-    print(body)
 
-# Build dict for JSONification.
-lastRecord = {"dts": dts, \
+    def __init__(self):
+        self.dl = owsData() # Data layer
+        self.modeJson = False # Default to JSON mode.        
+    
+    def __htmlify(self, weatherDict):
+        """
+        htmlify(weatherDict)
+        
+        Convert weather data to an HTML page. Returns a string.
+        """
+        # HTML header
+        body = "<HTML>\n<HEAD>\n<TITLE>OpenWeatherStn live data</TITLE>\n</HEAD>\n<BODY>\n"
+        # Build page with data.
+        # HTML footer
+        body = body + "</BODY>\n</HTML>"
+        
+        # Return page
+        return body
+    
+    def worker(self, env, startResponse):
+        """
+        worker(evn, startResponse)
+        
+        Do all the things. Accepts two arguments: the environment data, and start_server from WSGI.
+        """
+        
+        # Grab our enviornment data
+        checkEnv = env.copy()
+        
+        # Dummy string to hold the text body.
+        body = ""
+        
+        # Pull the most recent record from the data layer.
+        lastRecord = self.dl.getLastRecord()
+        
+        # Grab the date time stamp for processing as a string.
+        dts = str(lastRecord[0])
+        
+        # Zero-fill the string in the event we don't have ms specified.
+        # For consistency.
+        if len(dts) == 19:
+            dts = dts + ".000000"
+            
+        # Build dict for JSONification.
+        lastRecord = {"dts": dts, \
             "temp": {"value": lastRecord[1], "unit": "C"}, \
             "humidity": {"value": lastRecord[2], "unit": "%RH"}, \
             "baro": {"value": lastRecord[3], "unit": "kPa"}, \
@@ -58,14 +74,48 @@ lastRecord = {"dts": dts, \
             "windMaxSpd": {"value": lastRecord[7], "unit": "kph"}, \
             "lightAmb": {"value": lastRecord[8], "unit": "scalar"}, \
             "sysTemp": {"value": lastRecord[9], "unit": "C"}}
+        
+        # JSON or HTML mode?
+        if checkEnv['REQUEST_METHOD'] == 'POST':
+            # Build JSON string from dict.
+            jsonRecord = json.dumps(lastRecord)
+            
+            # Dump JSON MIME type:
+            cntntType = "application/javascript"
+            
+            # Set body.
+            body = jsonRecord
+        else:
+            # Dump HTML MIME type
+            cntntType = "text/html"
+            
+            # Gather HTML
+            body = self.__htmlify(lastRecord)
+        
+        # Set a generic HTTP 200 status
+        status = "200 OK"
+        
+        # Set content type header
+        headers = [('Content-Type', cntntType)]
+        
+        # Start returning the page
+        startResponse(status, headers)
+        
+        # Send the output back to our web server.
+        return [bytes(body, 'utf-8')]
 
 
-# JSON or HTML mode?
-if(modeJson):
-    # Build JSON string from dict.
-    jsonRecord = json.dumps(lastRecord)
+#######################
+# Main execution body #
+#######################
 
-    # Dump JSON string.
-    print(jsonRecord)
-else:
-    htmlify(lastRecord)
+# Utilize our worker class
+hardWorker = weatherService()
+
+# Validator
+validatorApp = validator(hardWorker.worker)
+
+# Set up HTTP server
+httpSrv = make_server('', 80, validatorApp)
+print("weatherService HTTP server listening on port 80.")
+httpSrv.serve_forever()
