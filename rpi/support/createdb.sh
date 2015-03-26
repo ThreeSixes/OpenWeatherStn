81693a0084d514ae45085c8aa00c0bb229abbaac
ThreeSixes <actual@joshlucy.com>
2015-03-26 17:08:32 -0400

    Set up script to create database directory and database in the directory above.

--- rpi/support/createdb.sh
+++ rpi/support/createdb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir db
-sqlite3 db/weather.db '.read createWeather.sql'
-chmod 755 db
-chmod 644 db/weather.db
+mkdir ../db
+sqlite3 ../db/weather.db '.read createWeather.sql'
+chmod 755 ../db
+chmod 644 ../db/weather.db
